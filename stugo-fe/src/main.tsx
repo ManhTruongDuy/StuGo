3f5f0604459b8a199aac94c543d46dfa718b4209
ManhTruongDuy <truongduymanh2512@gmail.com>
2026-06-08 16:35:20 +0700

    add analytics

--- stugo-fe/src/main.tsx
+++ stugo-fe/src/main.tsx
@@ -2,11 +2,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { SpeedInsights } from '@vercel/speed-insights/react'
+import { Analytics } from '@vercel/analytics/react'
 
 createRoot(document.getElementById('root')!).render(
   <>
     <App />
     <SpeedInsights />
+    <Analytics />
   </>
 )
+
 
