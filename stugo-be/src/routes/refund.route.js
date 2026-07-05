ff4104cf28146e298e78d46d90826def054241a5
ManhTruongDuy <truongduymanh2512@gmail.com>
2026-07-05 23:20:38 +0700

    fix: change auth to authenticate in refund.route.js

--- stugo-be/src/routes/refund.route.js
+++ stugo-be/src/routes/refund.route.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import { getRefundRequests, reviewRefundRequest } from '../controllers/refund.controller.js';
-import { auth, authorize } from '../middlewares/auth.middleware.js';
+import { authenticate, authorize } from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
 
 // Admin only routes
-router.use(auth, authorize('admin'));
+router.use(authenticate, authorize('admin'));
 
 router.get('/', getRefundRequests);
 router.post('/:id/review', reviewRefundRequest);
